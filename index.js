ccc125ffb95c4c283caabf3b7669c4a9c3bd5b9c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-21 13:31:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const drivers = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendDriver(){
 }
 
+function destructivelyPrependDriver(){
+}
+
+function destructivelyAppendDriver(){
+}
+
 function appendDriver(){
 }
